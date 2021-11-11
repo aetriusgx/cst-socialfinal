f3d79663ec31c948cd424f8422855858f83503d3
kovuko <hern3629@gmail.com>
2021-11-11 10:25:08 -0800

    Mockup user example

---
+++ app/src/users.py
@@ -0,0 +1,7 @@
+mockup_users = {
+	"username": "",				# Without @
+	"description": "",			# User bio
+	"posts": {					# User posts
+		"654717983": ["", 0, 0],	# Random post ID: [Post text, likes, shares]
+	}
+}
