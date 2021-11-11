6c0cde2814937fb82d9762395b0a4c6f7b6f8922
kovuko <hern3629@gmail.com>
2021-11-11 13:05:14 -0800

    env prep

--- app/src/users.py
+++ app/src/users.py
@@ -5,3 +5,42 @@ mockup_users = {
 		"654717983": ["", 0, 0],	# Random post ID: [Post text, likes, shares]
 	}
 }
+
+'''
+	Username requests:
+		kovuuko
+		leafrgb
+		vortex
+		efuentes
+		
+		Randomly generated:
+			layercord
+			bakercrepe
+			perhapsthree
+			sparrowparent
+			foodinjury
+			pythondonkey
+			pumpkinannoyed
+			andorrantapir
+			
+
+'''
+
+'''
+	Post requests:
+		Minecraft splashes:
+			Truly gone fishing!
+			Thank you for fish!
+			Something's not quite right...
+			The true meaning of covfefe. An illusion!
+			Don't feed chocolate to parrots
+			Throw yourself at the ground and miss
+			Rule #1, it's never my fault
+			Where there is not light, there is spider!
+			Any computer is a laptop if you're brave enough!
+			Stop being reasonable, this is the internet!
+			Yay, puppies for everyone!
+			One day, somewhere, my work will be quoted!
+			if not ok then return end
+			Follow the train, CJ!
+'''
