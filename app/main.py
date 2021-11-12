496fa2f8425af24de8b603762217358d7220afaf
Josue Santos <santozjosue831@gmail.com>
2021-11-11 21:06:37 -0800

    Added a functional Nav bar
    - Added search functionality to find a user.
    - Added a new route call /view_profiles
    - If user is found it prints the data in the /view_profiles route
    NOTE: Just a concept can be changed later

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from PIL import Image
+from src import users
 import random
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+user_data = users.mockup_users
+
 @app.route('/')
 def homePage():
 	return render_template('home.html')
+
+@app.route('/view_profiles', methods=['POST'])
+def view_profiles():
+	user = request.form.get('search-user')
+	return render_template('profile.html', user=user, user_data=user_data)
