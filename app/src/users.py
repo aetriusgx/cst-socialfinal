78161d5cb7031457998fb350fd66567f0e9a648d
LeafRGB <josed21st@gmail.com>
2021-12-11 21:20:08 -0800

    Updated user post file

--- app/src/users.py
+++ app/src/users.py
@@ -76,6 +76,76 @@ mockup_users = [
 		"377299112": ["The true meaning of covfefe. An illusion!", 0, 0],
 		"077165462": ["Throw yourself at the ground and miss", 0, 0],
 		"948635422": ["Stop being reasonable, this is the internet!", 0, 0]
+	}},
+	{"username": "i_am_midi",									# Without @
+	"description": "Avril Lavigne stan",		               	# User bio
+	"posts": {										 		  	# User posts
+		"987132313": ["Hey hey, you you, I don't like your girlfriend!", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"165916545": ["I reject your reality and substitute my own", 0, 0],
+		"077132362": ["He was a skater boy, wait do people even skate still?", 0, 0],
+		"949724322": ["Is that blue or purple, I really can't tell. Or is it blurple?", 0, 0],
+		"244644688": ["My eyes don't work", 0, 0]
+	}},
+	{"username": "Leaf",								# Without @
+	"description": "Nerd with too many hobbies, any pronouns!",		               			# User bio
+	"posts": {										 		  	# User posts
+		"062169420": ["There is an obscene amount of furries who are sys admins", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"062188787": ["Help my 5900X is melting again", 0, 0],
+		"062194390": ["I lost *another* 10mm socket", 0, 0],
+		"062190707": ["Hard drives go brrrrrrrrrrrrr...", 0, 0],
+		"062189879": ["Day 3621 of Windows being pain", 0, 0],
+		"062166858": ["It is cold, that means I must go for records on 3DMark", 0, 0],
+		"062187684": ["Want to hear my new song?"]
+	}},
+	{"username": "itsyoboyray",								# Without @
+	"description": "Being Human is hard",		               			# User bio
+	"posts": {										 		  	# User posts
+		"574225443": ["There is like a 90%\\ chance I'm listening to Chelsea Cutler rn", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"897480319": ["I'm not getting off Valorant until I win a game, this could take a while", 0, 0],
+		"084289543": ["Wait RLCS just started huh", 0, 0],
+		"843834343": ["S2000's are so cool", 0, 0],
+		"088989248": ["iS tHaT a sUpRa?!?!1?!/!", 0, 0],
+		"464644841": ["I have never seen a greated dub than this one bossman", 0, 0]
+	}},
+	{"username": "Siixes",								# Without @
+	"description": "Hi :)",		               			# User bio
+	"posts": {										 		  	# User posts
+		"481654165": ["Its surprising how hard it is to get people together for a picture", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"989788323": ["My cats are absolutely adorable", 0, 0],
+		"874270428": ["The strut for the hatch on my car needs replacing :(", 0, 0],
+		"097437924": ["Enjoying lunch outside in this wonderful weather", 0, 0],
+		"656424343": ["Got a haircut today", 0, 0],
+		"894323272": ["My door seal is leaking :(", 0, 0]
+	}},
+	{"username": "Zoomer871",								# Without @
+	"description": "I am increadibily boring, hi",		               			# User bio
+	"posts": {										 		  	# User posts
+		"764274980": ["Oh no Zee released another truck sound", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"974279438": ["Where did my earbuds go", 0, 0],
+		"313142453": ["My USB cable is broken", 0, 0],
+		"432425388": ["Type C is best cable", 0, 0],
+		"888035908": ["My internet is less than 1mbps I love it here", 0, 0],
+		"215354119": ["I saw a cool car on my way back from Melbourne", 0, 0]
+	}},
+	{"username": "Rizentral",								# Without @
+	"description": "I build terrible cars and make LED car badges",		               			# User bio
+	"posts": {										 		  	# User posts
+		"794793283": ["IM THE DOUCHIEST CHRISTMAS TREE, HO HO HO, YEET YEET YEET. SOMEONE TOOK MY TAPE FROM ME AND TOOK IT HOME WITH THEM", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"804804380": ["Its a root beer purple Passat I'm gonna hAVE PTSD", 0, 0],
+		"893189309": ["So aside from the everything not being bolted on, the car \"works\" great!", 0, 0],
+		"813139090": ["I have a bluetooth driveshaft", 0, 0],
+		"809213892": ["Pwaf", 0, 0],
+		"080813332": ["My starter got so hot it shorted to the frame lol", 0, 0]
+	}},
+	{"username": "Sliceable",								# Without @
+	"description": "Subaru Mechanic",		               			# User bio
+	"posts": {										 		  	# User posts
+		"984342434": ["Help I've binned it in the nasho", 0, 0],  			# 9 digit Random post ID: [Post text, likes, shares]
+		"464646546": ["Its only the NA Subaru headgaskets that like going bye bye", 0, 0],
+		"145367831": ["Volvo trim removal tool > Any other trim removal tool", 0, 0],
+		"485285573": ["My cat is sitting on my Xbox, if I overheat I'm sorry", 0, 0],
+		"898942893": ["I now have a longboi 10mm socket", 0, 0],
+		"321564845": ["Mmmmm, Japanese cars", 0, 0]
 	}}
 ]
 
